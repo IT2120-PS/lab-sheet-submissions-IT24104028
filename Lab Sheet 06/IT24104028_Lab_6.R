8063c76d6deb7e832840487ac970cf3359844a06
IT24104028 <it24104028@my.sliit.lk>
2025-09-15 12:36:56 +0530

    LabSheet_06

---
+++ Lab Sheet 06/IT24104028_Lab_6.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\CHAMA COMPUTERS\\OneDrive\\Desktop\\IT24104028")
+
+#part 1
+#i
+#Binomial Distribution
+
+#ii
+1- pbinom(47,50,0.85,lower.tail =TRUE)-pbinom(47,50,0.85,lower.tail =FALSE)
+
+#part 2
+#i X = the number of customer calls received in an hour.
+#ii. poisson distribution
+#iii
+dpois(15,12)
+
